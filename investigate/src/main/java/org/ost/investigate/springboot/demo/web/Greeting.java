901758a2bae5d4a513645ad1f6dc7dc2d418012b
Ostap Seniuk <Ostap_Seniuk@epam.com>
2020-06-17 15:24:28 +0300

    Greeting

---
+++ investigate/src/main/java/org/ost/investigate/springboot/demo/web/Greeting.java
@@ -0,0 +1,10 @@
+package org.ost.investigate.springboot.demo.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    private final String content;
+}
