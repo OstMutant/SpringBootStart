f30ae29d07e054f349a364cf4d5333c333ae892b
Ostap Seniuk <Ostap_Seniuk@epam.com>
2020-07-01 14:01:10 +0300

    .

--- investigate/src/main/java/org/ost/investigate/springboot/demo/dto/Greeting.java
+++ investigate/src/main/java/org/ost/investigate/springboot/demo/dto/Greeting.java
@@ -3,7 +3,6 @@ package org.ost.investigate.springboot.demo.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
